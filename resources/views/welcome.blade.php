79a11ae5a6d28060109054aae977cc0f30d37024
Mohit Punjabi <me@mohitpunjabi.com>
2015-05-18 15:42:37 +0530

    Made under construction page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>Nitya - Eternal Fashion</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
+        <link rel="icon" type="image/ico" href="{{ asset('favicon.png') }}" />
 		<style>
 			body {
 				margin: 0;
 				padding: 0;
 				width: 100%;
 				height: 100%;
-				color: #B0BEC5;
+				color: #444;
 				display: table;
 				font-weight: 100;
 				font-family: 'Lato';
@@ -35,13 +35,18 @@
 			.quote {
 				font-size: 24px;
 			}
+
+            .img {
+                max-width: 100%;
+                width: 100%;
+            }
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title"><img class="img" src="{{ asset('img/nitya-logo.jpg') }}" /></div>
+				<div class="quote">The website is under construction.</div>
 			</div>
 		</div>
 	</body>
